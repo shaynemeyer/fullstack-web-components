392bc4b7622b062f178535c8c1ccd62025cd95f7
Shayne Meyer <shayne.meyer@gmail.com>
2022-06-18 15:30:39 -0700

    feat: card styling

--- packages/component/src/card/Card.ts
+++ packages/component/src/card/Card.ts
@@ -5,15 +5,40 @@ export class CardComponent extends HTMLElement {
 
     const template = document.createElement('template');
     template.innerHTML = `
-        <header>
-          <slot name="header"></slot>
-        </header>
-        <section>
-          <slot name="content"></slot>
-        </section>
-        <footer>
-          <slot name="footer"></slot>
-        </footer>
+      <style>
+        :host {
+          display: block;
+          background: var(--color-white);
+          border-radius: var(--radius-md);
+          box-shadow: var(--shadow);
+          overflow: hidden;
+          max-width: 320px;
+        }
+        ::slotted(*) {
+          padding-left: var(--padding-lg);
+          padding-right: var(--padding-lg);
+        }
+        ::slotted(a:link), ::slotted(a:visited) {
+          display: block;
+        }
+        ::slotted(:last-child) {
+          padding-bottom: var(--margin-lg);
+        }
+        ::slotted(img) {
+          width: 100%;
+          padding-left: 0px;
+          padding-right: 0px;
+        }      
+      </style>
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <section>
+        <slot name="content"></slot>
+      </section>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
     `;
 
     shadowRoot.appendChild(template.content.cloneNode(true));
